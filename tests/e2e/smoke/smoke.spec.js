02be8ae382082eac6aca30acdda7a0d0d902a720
Sam-WardPickering <samuel.wardpickering@gmail.com>
2026-02-23 17:27:25 +1300

    Add smoke test code

--- tests/e2e/smoke/smoke.spec.js
+++ tests/e2e/smoke/smoke.spec.js
@@ -2,5 +2,7 @@ import { test, expect } from '../../fixtures/baseTest.js';
 import { gotoApp } from '../../helpers/navigation.js';
 
 test('loads login screen', async ({ page }) => {
-    
+    await gotoApp(page);
+
+    await expect(page.getByTestId('login-card')).toBeVisible();
 });
