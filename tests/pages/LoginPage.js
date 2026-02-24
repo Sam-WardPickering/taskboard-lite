708ea932ad8c902a2d11d8a42abbf99ba2b386e9
Sam-WardPickering <samuel.wardpickering@gmail.com>
2026-02-24 14:40:48 +1300

    Add constructor data for LoginPage

--- tests/pages/LoginPage.js
+++ tests/pages/LoginPage.js
@@ -5,5 +5,11 @@ export class LoginPage {
      */
     constructor(page) {
         this.page = page;
+
+        this.card = page.getByTestId('login-card');
+        this.emailInput = page.getByTestId('login-email');
+        this.passwordInput = page.getByTestId('login-password');
+        this.signInButton = page.getByTestId('login-submit');
+        this.signInError = page.getByTestId('login-error');
     }
 };
