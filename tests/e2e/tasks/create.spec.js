9995f7808dfddc9aa6adfe337b1ab29bd1eabc5b
Sam-WardPickering <samuel.wardpickering@gmail.com>
2026-02-26 14:43:27 +1300

    Create test file for creating tasks

---
+++ tests/e2e/tasks/create.spec.js
@@ -0,0 +1,8 @@
+import { test, expect } from '../../fixtures/baseTest.js';
+import { gotoApp } from '../../helpers/navigation.js';
+import { LoginPage } from '../../pages/LoginPage.js';
+import { TaskBoardPage } from '../../pages/TaskBoardPage';
+
+test.describe('Create', () => {
+
+});
