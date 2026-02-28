afa866d435c8f89647e097aa089ead6379c31312
Sam-WardPickering <samuel.wardpickering@gmail.com>
2026-02-28 23:13:33 +1300

    Create init file for task deletion tests

---
+++ tests/e2e/tasks/delete.spec.js
@@ -0,0 +1,4 @@
+import { test, expect } from '../../fixtures/baseTest.js';
+import { gotoApp } from '../../helpers/navigation.js';
+import { LoginPage } from '../../pages/LoginPage.js';
+import { TaskBoardPage } from '../../pages/TaskBoardPage';
