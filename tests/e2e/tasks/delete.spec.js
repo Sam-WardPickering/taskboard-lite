44089ff90a7ab505355a2dc9b45c66c5fec2f977
Sam-WardPickering <samuel.wardpickering@gmail.com>
2026-02-28 23:16:35 +1300

    Set up first delete test, add variables and page objects

--- tests/e2e/tasks/delete.spec.js
+++ tests/e2e/tasks/delete.spec.js
@@ -2,3 +2,14 @@ import { test, expect } from '../../fixtures/baseTest.js';
 import { gotoApp } from '../../helpers/navigation.js';
 import { LoginPage } from '../../pages/LoginPage.js';
 import { TaskBoardPage } from '../../pages/TaskBoardPage';
+
+test.describe('Delete', () => {
+    test('task can be deleted (happy path)', async ({ page }) => {
+        const email = 'sam@test.com';
+        const password = 'password123';
+        const expectedUser = email.split('@')[0];
+
+        const login = new LoginPage(page);
+        const taskBoard = new TaskBoardPage(page);
+    });
+});
