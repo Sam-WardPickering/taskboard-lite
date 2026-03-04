18f1f8a7235d682ba45ee2d7b2ee85cd872b7ea3
Sam-WardPickering <samuel.wardpickering@gmail.com>
2026-03-04 14:16:38 +1300

    Create rough edit task test

--- tests/e2e/tasks/edit.spec.js
+++ tests/e2e/tasks/edit.spec.js
@@ -4,7 +4,7 @@ import { todayISO } from '../../helpers/date.js';
 import { LoginPage } from '../../pages/LoginPage.js';
 import { TaskBoardPage } from '../../pages/TaskBoardPage';
 
-test.describe('Tasks - Edit', () => {
+test.describe.only('Tasks - Edit', () => {
     const email = 'sam@test.com';
     const password = 'password123';
     const expectedUser = email.split('@')[0];
@@ -13,10 +13,26 @@ test.describe('Tasks - Edit', () => {
         const title = `Task ${Date.now()}`;
         const due = todayISO();
         const priority = 'high';
+        const newTitle = `Edited Title - ${title}`;
 
-        gotoApp(page);
+        await gotoApp(page);
 
-        
+        const login = new LoginPage(page);
+        const taskBoard = new TaskBoardPage(page);
+
+        await login.login(email, password);
+
+        await expect(taskBoard.card).toBeVisible();
+        await expect(taskBoard.userName).toHaveText(expectedUser);
+
+        await taskBoard.createTask({ title, due, priority });
+
+        await expect(taskBoard.taskItem(title)).toBeVisible();
+
+        await taskBoard.editTask(title, { title: newTitle });
+
+        await expect(taskBoard.taskItem(title)).toHaveCount(0);
+        await expect(taskBoard.taskItem(newTitle)).toBeVisible();
 
     });
 });
