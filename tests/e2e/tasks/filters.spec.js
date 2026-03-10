f2a5a1c522a81ce4e116c540a245d19c29bce501
Sam-WardPickering <samuel.wardpickering@gmail.com>
2026-03-10 13:33:15 +1300

    Update filters.spec to reference user object

--- tests/e2e/tasks/filters.spec.js
+++ tests/e2e/tasks/filters.spec.js
@@ -3,6 +3,9 @@ import { gotoApp } from '../../helpers/navigation.js';
 import { todayISO } from '../../helpers/date.js';
 import { LoginPage } from '../../pages/LoginPage.js';
 import { TaskBoardPage } from '../../pages/TaskBoardPage.js';
+import { testUsers } from '../../test-data/users.js';
+
+const user = testUsers.sam;
 
 test.describe('Task - Filters', () => {
     test('active filter', async ({ page }) => {
