74571742833eff29766dbbfde41b255138fffd6c
Sam-WardPickering <samuel.wardpickering@gmail.com>
2026-03-10 13:10:53 +1300

    Create test file for filter testing

---
+++ tests/e2e/tasks/filters.spec.js
@@ -0,0 +1,11 @@
+import { test, expect } from '../../fixtures/baseTest.js';
+import { gotoApp } from '../../helpers/navigation.js';
+import { todayISO } from '../../helpers/date.js';
+import { LoginPage } from '../../pages/LoginPage.js';
+import { TaskBoardPage } from '../../pages/TaskBoardPage.js';
+
+test.describe('Task - Filters', () => {
+    test('active filter', async ({ page }) => {
+        
+    });
+});
