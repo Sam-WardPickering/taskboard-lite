c04da5bb922fa397afa56c4f9f7834d398c1901b
Sam-WardPickering <samuel.wardpickering@gmail.com>
2026-03-12 15:35:20 +1300

    Set up initial active filter test

--- tests/e2e/tasks/filters.spec.js
+++ tests/e2e/tasks/filters.spec.js
@@ -4,37 +4,40 @@ import { todayISO } from '../../helpers/date.js';
 import { LoginPage } from '../../pages/LoginPage.js';
 import { TaskBoardPage } from '../../pages/TaskBoardPage.js';
 import { testUsers } from '../../test-data/users.js';
+import { uniqueTitle } from '../../helpers/id.js';
 
 const user = testUsers.sam;
 
 test.describe('Task - Filters', () => {
-    // test('active filter', async ({ page }) => {
-    //     const taskActive = 'Task Active';
-    //     const taskCompleted = 'Task Completed';
+    test.only('active filter', async ({ page }) => {
+        const taskActive = uniqueTitle('Task Active');
+        const taskCompleted = uniqueTitle('Task Completed');
 
-    //     await gotoApp(page);
+        await gotoApp(page);
 
-    //     const login = new LoginPage(page);
-    //     const taskBoard = new TaskBoardPage(page);
+        const login = new LoginPage(page);
+        const taskBoard = new TaskBoardPage(page);
 
-    //     await login.login(user.email, user.password);
-    //     await expect(taskBoard.card).toBeVisible();
-    //     await expect(taskBoard.userName).toHaveText(user.expectedUser);
+        await login.login(user.email, user.password);
+        await expect(taskBoard.card).toBeVisible();
+        await expect(taskBoard.userName).toHaveText(user.expectedUser);
 
-    //     await taskBoard.createTask({ taskActive });
-    //     await taskBoard.createTask({ taskCompleted });
+        await taskBoard.createTask({ title: taskActive });
+        await expect(taskBoard.taskItem(taskActive)).toBeVisible();
 
+        await taskBoard.createTask({ title: taskCompleted });
+        await expect(taskBoard.taskItem(taskCompleted)).toBeVisible();
 
+        await taskBoard.toggleTask(taskCompleted);
+        
+        await expect(taskBoard.taskCheckbox(taskActive)).not.toBeChecked();
+        await expect(taskBoard.taskCheckbox(taskCompleted)).toBeChecked();
+        
+        // mark one completed
+        // navigate to active
+        // confirm one is there
 
-
-
-
-    //     // create 2 tasks
-    //     // mark one completed
-    //     // navigate to active
-    //     // confirm one is there
-
-    // });
+    });
     // test('completed filter', async ({ page }) => {
 
     // });
