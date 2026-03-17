15cabd132e9dba0342413aad1873892333762d8b
Sam-WardPickering <samuel.wardpickering@gmail.com>
2026-03-17 20:56:11 +1300

    Add complete task persistence tests to spec

--- tests/e2e/tasks/complete.spec.js
+++ tests/e2e/tasks/complete.spec.js
@@ -33,33 +33,4 @@ test.describe('Tasks - Complete', () => {
         await expect(taskBoard.taskCheckbox(title)).not.toBeChecked();
 
     });
-
-    test('completion persists after reload', async ({ page }) => {
-        const title = uniqueTitle();
-        const due = todayISO();
-        const priority = 'med';
-
-        await gotoApp(page);
-
-        const { taskBoard } = await loginAs(page, user);
-
-        await expect(taskBoard.card).toBeVisible();
-        await expect(taskBoard.userName).toHaveText(user.expectedUser);
-
-        await taskBoard.createTask({ title, due, priority });
-
-        await expect(taskBoard.taskItem(title)).toBeVisible();
-
-        // Complete task
-        await taskBoard.toggleTask(title);
-        await expect(taskBoard.taskCheckbox(title)).toBeChecked();
-        await expect(taskBoard.taskItem(title)).toHaveClass(/completed/);
-
-        await page.reload();
-
-        await expect(taskBoard.card).toBeVisible();
-        await expect(taskBoard.userName).toHaveText(user.expectedUser);
-        await expect(taskBoard.taskItem(title)).toBeVisible();
-        await expect(taskBoard.taskCheckbox(title)).toBeChecked();
-    });
 });
