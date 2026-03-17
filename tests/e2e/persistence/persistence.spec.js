ad7ad016c795e4143c57fe916eea7b1fdfc5aa8e
Sam-WardPickering <samuel.wardpickering@gmail.com>
2026-03-17 20:53:45 +1300

    Create spec file to migrate all persistence tests to

---
+++ tests/e2e/persistence/persistence.spec.js
@@ -0,0 +1,8 @@
+import { test, expect } from '../../fixtures/baseTest.js';
+import { LoginPage } from '../../pages/LoginPage.js';
+import { TaskBoardPage } from '../../pages/TaskBoardPage.js';
+import { gotoApp } from '../../helpers/navigation.js';
+
+test.describe('Persistence Tests', () => {
+    
+})
