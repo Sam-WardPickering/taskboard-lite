0662ce2f5fd67de27fb70fc2780ce597c431334d
Sam-WardPickering <samuel.wardpickering@gmail.com>
2026-03-18 16:59:56 +1300

    Add initial test block

---
+++ tests/e2e/filters/search.spec.js
@@ -0,0 +1,11 @@
+import { test, expect } from '../../fixtures/baseTest.js';
+import { gotoApp } from '../../helpers/navigation.js';
+import { testUsers } from '../../test-data/users';
+import { uniqueTitle } from '../../helpers/id';
+import { loginAs } from '../../helpers/auth.js';
+
+const user = testUsers.sam;
+
+test.describe('Tasks - Search', () => {
+
+});
