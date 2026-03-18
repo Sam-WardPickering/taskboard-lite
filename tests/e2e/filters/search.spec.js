9a9d39940e264a2797315e6f6da0d68cdf3a1644
Sam-WardPickering <samuel.wardpickering@gmail.com>
2026-03-19 11:04:07 +1300

    Set up test 1

--- tests/e2e/filters/search.spec.js
+++ tests/e2e/filters/search.spec.js
@@ -6,6 +6,19 @@ import { loginAs } from '../../helpers/auth.js';
 
 const user = testUsers.sam;
 
-test.describe('Tasks - Search', () => {
+test.describe('Tasks - Search', () => { 
+    test('filtering valid task value returns corresponding task', async ({ page }) => {
+        const task1 = 'Task One';
+        const task2 = 'Task Two';
+        
+        await gotoApp(page);
 
+        const { taskBoard } = await loginAs(page, user);
+
+        await expect(taskBoard.card).toBeVisible();
+        await expect(taskBoard.userName).toHaveText(user.expectedUser);
+
+        await taskBoard.createTask({title: task1});
+        await expect(taskBoard.taskItem(task1)).toBeVisible();
+    });
 });
