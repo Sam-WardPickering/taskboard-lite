969895246e4b1008df1ca0e787f7f6a3a584ee13
Sam-WardPickering <samuel.wardpickering@gmail.com>
2026-03-27 18:24:10 +1300

    Create tasks for test one

--- tests/e2e/tasks/bulk.spec.js
+++ tests/e2e/tasks/bulk.spec.js
@@ -1,4 +1,4 @@
-import { page, expect } from '../../fixtures/baseTest.js';
+import { test, expect } from '../../fixtures/baseTest.js';
 import { gotoApp } from '../../helpers/navigation.js';
 import { loginAs } from '../../helpers/auth.js';
 import { uniqueTitle } from '../../helpers/id.js';
@@ -7,7 +7,24 @@ import { testUsers } from '../../test-data/users.js';
 const user = testUsers.sam;
 
 test.describe('Tasks - Bulk Actions', () => {
-    test('all tasks are completed when Complete All is pressed', ({ page }) => {
+    test('all tasks are completed when Complete All is pressed', async ({ page }) => {
+        const taskOne = uniqueTitle('Task One');
+        const taskTwo = uniqueTitle('Task Two');
+        const taskThree = uniqueTitle('Task Three');
+
+        await gotoApp(page);
+
+        const { taskBoard } = await loginAs(page, user);
+
+        await expect(taskBoard.card).toBeVisible();
+        await expect(taskBoard.userName).toHaveText(user.expectedUser);
+
+        await taskBoard.createTask({ title: taskOne });
+        await taskBoard.createTask({ title: taskTwo });
+        await taskBoard.createTask({ title: taskThree });
+
+        
+
 
     });
 });
