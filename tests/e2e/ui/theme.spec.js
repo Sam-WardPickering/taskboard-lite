74484ae93f94f16df8959484650980f47db4b07d
Sam-WardPickering <samuel.wardpickering@gmail.com>
2026-03-31 09:41:29 +1300

    Add second ui theme test and refactor both

--- tests/e2e/ui/theme.spec.js
+++ tests/e2e/ui/theme.spec.js
@@ -1,21 +1,41 @@
 import { test, expect } from '../../fixtures/baseTest.js';
 import { gotoApp } from '../../helpers/navigation.js';
 import { AppShell } from '../../pages/AppShell.js';
+import { loginAs } from '../../helpers/auth.js';
+import { testUsers } from '../../test-data/users.js';
+
+const user = testUsers.sam;
 
 test.describe('UI - theme', () => {
-    test.only('theme changes while logged out', async ({ page }) => {
+    test('theme changes while logged out', async ({ page }) => {
         await gotoApp(page);
 
         const appShell = new AppShell(page);
 
-        expect(page.locator('html')).toHaveAttribute('data-theme', 'dark');
+        await expect(page.locator('html')).toHaveAttribute('data-theme', 'dark');
 
         await appShell.toggleTheme();
+        await expect(page.locator('html')).toHaveAttribute('data-theme', 'light');
 
-        expect(page.locator('html')).toHaveAttribute('data-theme', 'light');
+        await appShell.toggleTheme();
+        await expect(page.locator('html')).toHaveAttribute('data-theme', 'dark');
 
     });
     test('theme changes while logged in', async ({ page }) => {
+        await gotoApp(page);
 
+        const { taskBoard } = await loginAs(page, user);
+        const appShell = new AppShell(page);
+
+        await expect(taskBoard.card).toBeVisible();
+        await expect(taskBoard.userName).toHaveText(user.expectedUser);
+        
+        await expect(page.locator('html')).toHaveAttribute('data-theme', 'dark');
+
+        await appShell.toggleTheme();
+        await expect(page.locator('html')).toHaveAttribute('data-theme', 'light');
+
+        await appShell.toggleTheme();
+        await expect(page.locator('html')).toHaveAttribute('data-theme', 'dark');
     });
 });
