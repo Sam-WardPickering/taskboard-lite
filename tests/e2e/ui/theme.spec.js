5c8def580366dd2e6a6446604c8d3810352239ce
Sam-WardPickering <samuel.wardpickering@gmail.com>
2026-03-30 17:01:38 +1300

    Remove unused folders && create ui theme tests

---
+++ tests/e2e/ui/theme.spec.js
@@ -0,0 +1,13 @@
+import { describe } from 'node:test';
+import { test, expect } from '../../fixtures/baseTest.js';
+import { loginAs } from '../../helpers/auth.js';
+import { gotoApp } from '../../helpers/navigation.js';
+
+describe('UI - theme', () => {
+    test('theme changes while logged out', async ({ page }) => {
+
+    });
+    test('theme changes while logged in', async ({ page }) => {
+
+    });
+});
