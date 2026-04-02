112a58ec413a8a1d2a7fcef99d6cb7e83ef2021d
Sam-WardPickering <samuel.wardpickering@gmail.com>
2026-04-02 13:34:28 +1300

    Refactor create.spec tests to use new authenticatedPage fixture

--- tests/e2e/tasks/create.spec.js
+++ tests/e2e/tasks/create.spec.js
@@ -1,24 +1,12 @@
 import { test, expect } from '../../fixtures/baseTest.js';
-import { gotoApp } from '../../helpers/navigation.js';
 import { todayISO } from '../../helpers/date.js';
-import { testUsers } from '../../test-data/users.js';
 import { uniqueTitle } from '../../helpers/id.js';
-import { loginAs } from '../../helpers/auth.js';
-
-const user = testUsers.sam;
 
 test.describe('Tasks - Create', () => {
-    test('create a task (happy path)', async ({ page }) => {
+    test('create a task (happy path)', async ({ authenticatedPage: { taskBoard } }) => {
         const title = uniqueTitle();
         const due = todayISO();
         const priority = 'high';
-
-        await gotoApp(page);
-
-        const { taskBoard } = await loginAs(page, user);
-
-        await expect(taskBoard.card).toBeVisible();
-        await expect(taskBoard.userName).toHaveText(user.expectedUser);
 
         await taskBoard.createTask({ title, due, priority });
 
